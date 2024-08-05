1d336627ba794636c838d34ec6ccf877e550f39a
vanshverma721 <vvv.721781@gmail.com>
2024-08-05 13:02:45 +0530

    header component done

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,45 +1,40 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+
+const Header = styled(AppBar)`
+    z-index: 1201;
+    background: #fff;
+    height: 70px;
+    box-shadow: inset 0 -1px 0 0 #dadce0;
+`;
+
+const Heading = styled(Typography)`
+  color: #5F6368;
+  font-size: 24px;
+  margin-left: 25px;
+`
 
 const HeaderBar = ({ open, handleDrawer }) => {
+
+    const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
+
     return (
-        <AppBar position="fixed" open={open}>
+        <Header open={open}>
             <Toolbar>
                 <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
                     onClick={handleDrawer}
                     edge="start"
                     sx={{
-                        marginRight: 5,
-                        ...(open && { display: 'none' }),
+                        marginRight: '20px'
                     }}
                 >
-                    <MenuIcon />
+                    <Menu />
                 </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                    Mini variant drawer
-                </Typography>
+                <img src={logo} alt="logo" style={{width: 30}} />
+                <Heading>Keep</Heading>
             </Toolbar>
-        </AppBar>
+        </Header>
     )
 }
 
--- src/components/SwipeDrawer.jsx
+++ src/components/SwipeDrawer.jsx
@@ -3,21 +3,6 @@ import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import HeaderBar from './HeaderBar';
 import NavList from './NavList';
@@ -46,30 +31,8 @@ const closedMixin = (theme) => ({
 });
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
 }));
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
@@ -93,17 +56,13 @@ const SwipeDrawer = () => {
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
+    const handleDrawer = () => {
+        setOpen(prevState => !prevState);
     };
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <HeaderBar open={open} handleDrawer={handleDrawerOpen} />
+            <HeaderBar open={open} handleDrawer={handleDrawer} />
             <Drawer variant="permanent" open={open}>
                 <DrawerHeader></DrawerHeader>
                 <NavList open={open} />
