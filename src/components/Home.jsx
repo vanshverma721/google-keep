f6149f8f1a1334e87b39454189e4fa1e10830cd6
vanshverma721 <vvv.721781@gmail.com>
2024-08-05 12:30:40 +0530

    added swipe drawer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import SwipeDrawer from './SwipeDrawer';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <SwipeDrawer />
   )
 }
 
